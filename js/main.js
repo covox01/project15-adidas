1effd48a1f90f4bd8086e8d1f0a66e14a035117e
Dennis <oba.dennis@gmail.com>
2020-06-21 21:58:04 -0400

    Finalize

--- js/main.js
+++ js/main.js
@@ -1,26 +1,6 @@
-// var leafEase = Sine.easeOut
 var leafEase = Power3.easeInOut
 var leafEase2 = Power3.easeInOut
 var stripeEase = Sine.easeInOut
-
-// function start2(){
-//    var t = .6
-//    var tl = new TimelineMax()
-//    gsap.set("#leaf-right", {rotation: -42, x: -17, opacity: .9})
-//    tl.from("#leaf-right", .4, {y: -50, ease: Back.easeInOut.config(4)}, "sync")
-//       .from("#leaf-right", .4, {opacity: 0, ease: Power3.easeInOut}, "sync")
-//       .to("#leaf-right", .75, {rotation: 10, x: 0, opacity: 1, ease: Power3.easeInOut}, "+=.3")
-//       .to("#leaf-right", t, { rotation: 0, opacity: 1, ease: Power3.easeOut }, "-=.1")
-//       .from("#leaf-middle", t, { x: 20, rotation: 50, opacity: 0, ease: leafEase }, "-=.75")
-//       .from("#leaf-left", t, { x: 20, rotation: 80, opacity: 0, ease: leafEase }, "-=.75")
-//       .to(".leaf", .6, { opacity: 1 }, "-=.3")
-//       .staggerFromTo(".stripes-path", .5, { opacity: 0, x: -180 }, { opacity: 1, x: 0, ease: stripeEase }, .1, "-=.8")
-//       .staggerFromTo(".copy", t, { opacity: 0, y: }, { opacity: 1, ease: Power2.easeInOut }, .02, "-=.8")
-
-//    gsap.from(".copy", 1, {x: 40, ease: Power3.easeInOut, delay: .5})
-//    tl.timeScale(.8)
-//    gsap.delayedCall(3, end)
-// }
 
 function start1(){
    var t = .6
@@ -35,7 +15,6 @@ function start1(){
    gsap.from(".copy", 1, { y: 30, ease: Power3.easeInOut})
    gsap.delayedCall(2.5, end)
    tl.timeScale(.8)
-  
 }
 
 function end(){
@@ -44,16 +23,13 @@ function end(){
    tl
       .staggerTo(".copy", .5, { opacity: 0, y: 30, ease: Power2.easeInOut }, .04, "stagger")
       .staggerTo(".stripes-path", .75, { opacity: 0, y: 15, ease: Back.easeInOut.config(1) }, -.02, "stagger")
-      
-
       .to("#leaf-left", t, { rotation: -30, ease: leafEase2 }, "stagger")
       .to("#leaf-middle", t, { rotation: -70, x: -25, ease: leafEase2}, "stagger")
       .to("#leaf-right", t, {rotation: -110, x: -30, ease: leafEase2}, "stagger")
-      // .to(".leaf", .4, {opacity: 0, onComplete: clearProp}, "-=.4")
 
-   TweenMax.to(".leaf", .4, {opacity: 0, delay: .4})
-   TweenMax.to(".copy", 1, {y: 30, ease: Power3.easeInOut})
-   TweenMax.delayedCall(1.3, clearProps)
+   gsap.to(".leaf", .4, {opacity: 0, delay: .4})
+   gsap.to(".copy", 1, {y: 30, ease: Power3.easeInOut})
+   gsap.delayedCall(1.3, clearProps)
    tl.timeScale(1)
 }
 
@@ -63,23 +39,18 @@ function clearProps(){
 
 function set(){
    var checkTransform = gsap.getProperty("#leaf-right, #leaf-middle, #leaf-left", "opacity")
-   // gsap.set("#leaf-left", {opacity: .5, })
    if (checkTransform === 0) {
       gsap.delayedCall(.75, init)
    }
 }
 
-
-
 function init(){
-   
    gsap.set("#logo", {xPercent: -50, yPercent: -50})
    gsap.set("#leaf-left", {opacity: .5, transformOrigin: "bottom right", opacity: 1})
    gsap.set("#leaf-middle", { opacity: .7, transformOrigin: "bottom center", opacity: 1})
    gsap.set("#leaf-right", { opacity: 1, transformOrigin: "left bottom"})
    gsap.set(".stripes-path, .copy", {opacity: 0, force3D: false, rotation: 0.01})
    gsap.set("#stripes, #leaf-group", { force3D: false, rotation: 0.01, onComplete: start1 })
-
 }
 
 init()
